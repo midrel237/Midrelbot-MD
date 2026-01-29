b68d660296579747a7f0782765471ca485c437cd
midrel237 <131145810+midrel237@users.noreply.github.com>
2026-01-29 09:48:02 +0100

    Update bot owner and owner number in settings

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: '✎𝑴𝒊𝒅𝒓𝒆𝒍~𝑷𝒐𝒈𝒃𝒂℠', // Your name
+  ownerNumber: '237640077127', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
